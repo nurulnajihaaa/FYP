7c21f7077b6e84e5964ecadf99747932586ae6dd
nurulnajihaaa <nurulnajihahbb@gmail.com>
2024-12-09 10:05:30 +0800

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -13,11 +13,11 @@ if uploaded_file is not None:
     st.subheader("Uploaded Image")
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
-        # Convert the image to a NumPy array
+    st.write("Image successfully uploaded and processed as a NumPy array.")
+    # Convert the image to a NumPy array
     image_np = np.array(image)
 else:
     st.info("Please upload an image to display.")
-    st.write("Image successfully uploaded and processed as a NumPy array.")
 
 #else:
    # st.info("Please upload an image to display and convert.")
