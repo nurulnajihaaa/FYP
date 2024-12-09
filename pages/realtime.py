8a8cbb25a8fd1a0ac71efcb9c7a0c8d549a7a626
nurulnajihaaa <nurulnajihahbb@gmail.com>
2024-12-09 11:41:02 +0800

    Update realtime.py

--- pages/realtime.py
+++ pages/realtime.py
@@ -2,11 +2,11 @@ import streamlit as st
 import cv2
 import numpy as np
 
-enable = st.checkbox("Enable camera")
-picture = st.camera_input("Take a picture", disabled=not enable)
+#enable = st.checkbox("Enable camera")
+#picture = st.camera_input("Take a picture", disabled=not enable)
 
-if picture:
-    st.image(picture)
+#if picture:
+#    st.image(picture)
 
 
 img_file_buffer = st.camera_input("Take a picture")
