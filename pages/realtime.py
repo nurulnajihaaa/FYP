9bf4ecc2f0d88b120cf306945a14f18df8b5a7f2
nurulnajihaaa <nurulnajihahbb@gmail.com>
2024-12-09 11:28:08 +0800

    Update realtime.py

--- pages/realtime.py
+++ pages/realtime.py
@@ -7,7 +7,7 @@ FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
 
 while run:
-    _, frame = camera.read()
+    ret, frame = camera.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
 else:
